3f6515324d3cb41c4fd7668ab63170e16cb10290
Shiraz Muhammed <shirazmuhammed04@gmail.com>
2024-05-04 18:33:27 +0530

    home.jsx cleared

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,5 @@
 import React from 'react'
-import Button from '@mui/material/Button';
 import Header from '../components/Header';
-import { Hero } from '../components/Hero';
-import StudentLogin from './login/StudentLogin';
-import { OwnerLogin } from './login/OwnerLogin';
-import { AdminLogin } from './login/AdminLogin';
-import StudentSignup from './signup/StudentSignup';
-import OwnerSignup from './signup/OwnerSignup';
-import StudentHome from './StudentHome';
 
 
 const Home = (props) => {
